c39853d75be1caa26b9d316f57ab35caababd64c
Jkingb4 <jakobmatthewking@gmail.com>
2025-11-30 18:27:51 -0500

    Update chess_logic.js

--- chess_logic.js
+++ chess_logic.js
@@ -198,4 +198,5 @@ class Chessboard {
 
     // gotta make the function "isincheck" https://youtu.be/fJIsqZmQVZQ?t=1693 literally does it for us (link is timestamped)
 
+    module.exports = { Chessboard, Piece, Pawn, Rook, Knight, Bishop, Queen, King }; // Makes this a module, allows server to request (I think)
 }
