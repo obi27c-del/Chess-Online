71ee44c81f18ad41290f1a0efb27e82eb6d944a2
Jkingb4 <jakobmatthewking@gmail.com>
2025-11-30 18:26:18 -0500

    Update server.js
    adds a route to get current board state, also somehow figured out how to bring the logic file to the server

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const path = require('path');
 const app = express();
 const PORT = 4242;
 
+const { Chessboard } = require('./chesslogic');
+
+// Create a chessboard instance. for now, one per server, we would have to do per player at somepoint
+const chess = new Chessboard('w');
+
 // Route for the intro page
 app.get('/chess', (req, res) => {
     res.sendFile(path.join(__dirname, 'chessintro.html'));
@@ -13,6 +18,14 @@ app.get('/chess', (req, res) => {
 // Route for the game page
 app.get('/chess/game', (req, res) => {
     res.sendFile(path.join(__dirname, 'chessgame.html'));
+});
+
+// Send board state as JSON
+app.get('/chess/game/state', (req, res) => {
+    res.json({
+        board: chess.getCurrentBoard(),
+        playerColor: chess.getPlayerColor()
+    });
 });
 
 // Start the server.
