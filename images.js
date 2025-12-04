782f46506fb5e5ee92f3fa623c251cc2e1e95706
Jkingb4 <jakobmatthewking@gmail.com>
2025-12-04 01:23:52 -0500

    Create images.js

---
+++ images.js
@@ -0,0 +1,14 @@
+export const images = {
+    "p": "/blackPawn.png",
+    "r": "/blackRook.png",
+    "n": "/blackKnight.png",
+    "b": "/blackBishop.png",
+    "q": "/blackQueen.png",
+    "k": "/blackKing.png",
+    "P": "/whitePawn.png",
+    "R": "/whiteRook.png",
+    "N": "/whiteKnight.png",
+    "B": "/whiteBishop.png",
+    "Q": "/whiteQueen.png",
+    "K": "/whiteKing.png",
+};
